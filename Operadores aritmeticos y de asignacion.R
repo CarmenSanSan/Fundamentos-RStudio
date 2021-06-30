c880b607b1104dbf7fbdeac7f114a5d4da4e4a1f
´Carmen´ <sanchezscarmen98@outlook.com>
2021-06-30 10:48:17 -0500

    fhbhi

---
+++ Operadores aritmeticos y de asignacion.R
@@ -0,0 +1,18 @@
+sqrt(25)
+1*5
+26/3
+sumando_1 <- 6552684958+168454584
+restando_1 <- 31684164-16496461
+dividiendo_1 <- 31686564/6465165
+potenciando_1 <- 25489^23
+multiplicando <- 26841*619863
+sumando_2 <- 36841+654616
+restando_2 <- 686568-65462
+dividiendo_2 <- 654618/651654
+potenciando_2 <- 654268^2
+multiplicando_2 <- 54513*2684
+suma_1.1 <- sumando_1+sumando_2
+suma_1.1
+sumando_1
+igual <- "no usar tanto"
+
